de15f027ba1dca553eb54ce100c04724d9087602
s-light <github@s-light.eu>
2018-11-25 00:10:19 +0100

    split fancyled and 2d_array examples

--- examples/tlc5957_fancyled.py
+++ examples/tlc5957_fancyled.py
@@ -54,9 +54,7 @@ latch.direction = digitalio.Direction.OUTPUT
 ##########################################
 print(42 * '*')
 print("define pixel array / init TLC5957")
-rows = 4
-cols = 4
-num_leds = rows * cols
+num_leds = 16
 pixels = slight_tlc5957.TLC5957(
     spi=spi,
     latch=latch,
@@ -73,82 +71,6 @@ print("channel_count", pixels.channel_count)
 
 ##########################################
 # helper function
-
-def map_range(x, in_min, in_max, out_min, out_max):
-    """Map Value from one range to another."""
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
-
-
-def map_range_int(x, in_min, in_max, out_min, out_max):
-    """Map Value from one range to another."""
-    return int(
-        (x - in_min) * (out_max - out_min)
-        //
-        (in_max - in_min) + out_min
-    )
-
-
-##########################################
-# mapping function
-
-def get_pixel_index_from_row_col(row, col):
-    """Get pixel_index from row and column index."""
-    map = [
-        [15, 14, 13, 12],
-        [11, 10,  9,  8],
-        [7,   6,  5,  4],
-        [3,   2,  1,  0],
-    ]
-    pixel_index = map[row][col]
-    return pixel_index
-
-
-##########################################
-# print(42 * '*')
-# print("set colors")
-# for index in range(num_leds):
-#     pixels[index] = (1, 1, 1)
-# # write data to chips
-# pixels.show()
-# time.sleep(2)
-# print("set colors2")
-# for index in range(num_leds):
-#     pixels[index] = (0, 100, 1000)
-# # write data to chips
-# pixels.show()
-# time.sleep(2)
-
-##########################################
-print(42 * '*')
-print("set colors")
-# set first pixel to orange
-pixels[get_pixel_index_from_row_col(0, 0)] = (1.0, 0.5, 0.0)
-pixels[get_pixel_index_from_row_col(0, 3)] = (0.1, 0.0, 1.0)
-pixels[get_pixel_index_from_row_col(3, 0)] = (0.1, 0.5, 0.0)
-pixels[get_pixel_index_from_row_col(3, 3)] = (0.0, 0.5, 1.0)
-pixels.show()
-time.sleep(2)
-
-print("set color range")
-for x in range(cols):
-    # xN = x / cols
-    xN = map_range_int(x, 0, cols, 1, 100)
-    for y in range(rows):
-        # yN = y / rows
-        yN = map_range_int(y, 0, rows, 1, 100)
-        # print(
-        #     "x: {:>2} xN: {:>2} "
-        #     "y: {:>2} yN: {:>2} "
-        #     "pixel_index: {:>2}".format(
-        #         x, xN,
-        #         y, yN,
-        #         get_pixel_index_from_row_col(x, y)
-        #     )
-        # )
-        pixels[get_pixel_index_from_row_col(x, y)] = (xN, yN, 0)
-
-pixels.show()
-time.sleep(2)
 
 
 ##########################################
